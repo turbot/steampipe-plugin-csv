a3a23ac68d86724072785fc6fa106885c4a1a7dd
Ved misra <47312748+misraved@users.noreply.github.com>
2022-04-27 16:28:54 +0530

    Recompile plugin with steampipe-plugin-sdk-v3.1.0 and go v1.18 (#16)

--- csv/table_code_csv.go
+++ csv/table_code_csv.go
@@ -7,9 +7,9 @@ import (
 	"io"
 	"os"
 
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 func tableCSV(ctx context.Context, p *plugin.Plugin) (*plugin.Table, error) {
