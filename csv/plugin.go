062b6324d25f465cce28fb6441109243798aa384
sourav chakraborty <sourav@turbot.com>
2023-06-29 19:56:20 +0530

    WIP

--- csv/plugin.go
+++ csv/plugin.go
@@ -5,8 +5,10 @@ import (
 	"errors"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
+	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
@@ -42,10 +44,24 @@ func PluginTables(ctx context.Context, d *plugin.TableMapData) (map[string]*plug
 	if err != nil {
 		return nil, err
 	}
+
+	tableNames := []string{}
+	re := regexp.MustCompile(`[-.]`)
 	for _, i := range paths {
 		tableCtx := context.WithValue(ctx, keyPath, i)
 		base := strings.TrimSuffix(filepath.Base(i), gzipExtension)
-		tables[base[0:len(base)-len(filepath.Ext(base))]], err = tableCSV(tableCtx, d.Connection)
+
+		// check if the base name is already added as table name
+		// then add the parent folder as prefix to the base for table name
+		// if 2 same name file is present in 2 different path - /Users/dspl_countries.csv and /Users/test/dspl_countries.csv
+		// there will be 2 tables - dspl_countries and test_dspl_countries
+		if helpers.StringSliceContains(tableNames, base[0:len(base)-len(filepath.Ext(base))]) {
+			folder_path := re.ReplaceAllString(strings.Split(i, "/")[len(strings.Split(i, "/"))-2], "_")
+			tables[folder_path+"_"+base[0:len(base)-len(filepath.Ext(base))]], err = tableCSV(tableCtx, d.Connection)
+		} else {
+			tables[base[0:len(base)-len(filepath.Ext(base))]], err = tableCSV(tableCtx, d.Connection)
+			tableNames = append(tableNames, base[0:len(base)-len(filepath.Ext(base))])
+		}
 		if err != nil {
 			plugin.Logger(ctx).Error("csv.PluginTables", "create_table_error", err, "path", i)
 			return nil, err
