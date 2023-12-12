c6a451703a23047f8a684da5e61e137e3777e7e8
misraved <ved@turbot.com>
2023-12-12 14:17:16 +0530

    Update plugin's connection config variables to use hcl syntax

--- csv/connection_config.go
+++ csv/connection_config.go
@@ -2,30 +2,13 @@ package csv
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type csvConfig struct {
-	Paths     []string `cty:"paths" steampipe:"watch"`
-	Separator *string  `cty:"separator"`
-	Comment   *string  `cty:"comment"`
-	Header    *string  `cty:"header"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"paths": {
-		Type: schema.TypeList,
-		Elem: &schema.Attribute{Type: schema.TypeString},
-	},
-	"separator": {
-		Type: schema.TypeString,
-	},
-	"comment": {
-		Type: schema.TypeString,
-	},
-	"header": {
-		Type: schema.TypeString,
-	},
+	Paths     []string `hcl:"paths" steampipe:"watch"`
+	Separator *string  `hcl:"separator"`
+	Comment   *string  `hcl:"comment"`
+	Header    *string  `hcl:"header"`
 }
 
 func ConfigInstance() interface{} {
